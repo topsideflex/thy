1223e16478b9670d34869608f1e271f84a3ea0a3
topsideflex <79905193+topsideflex@users.noreply.github.com>
2025-10-27 23:12:51 -1000

    Update route paths for user, product, and payment

--- server.js
+++ server.js
@@ -15,11 +15,9 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .catch(err => console.error(err));
 
 // Routes
-const userRoutes = require('./userRoutes');
-const productRoutes = require('./productRoutes');
-const paymentRoutes = require('./paymentRoutes');
-
-
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+const paymentRoutes = require('./routes/paymentRoutes');
 
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
