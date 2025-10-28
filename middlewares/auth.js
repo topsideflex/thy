eace44aa3f75f8c0a93a8587640a641dd57372a4
topsideflex <79905193+topsideflex@users.noreply.github.com>
2025-10-27 23:14:51 -1000

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,16 +1,14 @@
 const jwt = require('jsonwebtoken');
 
-const auth = (req, res, next) => {
+module.exports = (req, res, next) => {
   const token = req.header('Authorization')?.replace('Bearer ', '');
   if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
-
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.user;
     next();
-  } catch (err) {
+  } catch {
     res.status(401).json({ message: 'Token is not valid' });
   }
 };
 
-module.exports = auth;
