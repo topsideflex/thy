c750f1555c6e2f3cf4b0275768c21aa9513475aa
topsideflex <79905193+topsideflex@users.noreply.github.com>
2025-10-27 23:14:12 -1000

    Refactor paymentRoutes to use updated auth middleware

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const auth = require('./auth');
-
+const auth = require('../middlewares/auth');
 
 router.post('/checkout', auth, async (req, res) => {
   const { amount, currency = 'usd' } = req.body;
-
   try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount,
-      currency
-    });
+    const paymentIntent = await stripe.paymentIntents.create({ amount, currency });
     res.json({ clientSecret: paymentIntent.client_secret });
   } catch (err) {
     console.error(err);
